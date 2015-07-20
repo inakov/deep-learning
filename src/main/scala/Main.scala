64b6dbe3ebb09444d5d4141b1da0e60827042866
inakov <ivannakov@hotmail.com>
2015-07-20 18:32:44 +0300

    - Basic NN with backprop

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-import breeze.linalg.{DenseMatrix, sum}
+import breeze.linalg.{DenseVector, DenseMatrix, sum}
 import breeze.numerics._
 import datasets.housing.MNIST
 import deep.learning.networks.Network
@@ -10,18 +10,15 @@ object Main {
 
   def main(args: Array[String]) {
 
-//    val m = DenseMatrix((0.54315592,  0.31597731), (0.68744078,  0.58687109))
-//    val m2 = DenseMatrix((0.04315592,  0.31597731), (0.68744078,  0.58687109))
-//    println(m. - m2)
+//    println(DenseVector.rand[Double](4, rand = breeze.stats.distributions.Gaussian(0,1)))
 
     val mnistDataset = MNIST.loadTrainingDataSet
     val trainingData = mnistDataset._1
 
     val validationData = mnistDataset._2
 
-    val net = new Network(List(784, 30, 10))
-    net.SGD(trainingData, 30, 10, 0.5, testData = validationData)
-
+    val net = new Network(List(784, 100, 10))
+    net.SGD(trainingData, 200, 10, 0.5, lmbda = 0.5, testData = validationData)
 
   }
 
--- src/main/scala/deep/learning/networks/Network.scala
+++ src/main/scala/deep/learning/networks/Network.scala
@@ -19,7 +19,7 @@ class Network(networkDefinition: List[Int]) {
   var biases: ArrayBuffer[DenseVector[Double]] = new ArrayBuffer[DenseVector[Double]]()
   var weights: ArrayBuffer[DenseMatrix[Double]] = new ArrayBuffer[DenseMatrix[Double]]()
 
-  for(y <- definition.drop(1).take(numberOfLayers-1)) biases += DenseVector.zeros[Double](y)
+  for(y <- definition.drop(1).take(numberOfLayers-1)) biases += DenseVector.rand[Double](y, rand = breeze.stats.distributions.Gaussian(0,1))
 
   /*
   * weights is initialized with uniformely sampled
@@ -57,7 +57,7 @@ class Network(networkDefinition: List[Int]) {
     result
   }
 
-  def SGD(trainingData: Seq[(DenseVector[Double],DenseVector[Double])], epochs: Int, miniBatchSize: Int, eta: Double, testData:Seq[(DenseVector[Double],DenseVector[Double])] = Nil): Unit ={
+  def SGD(trainingData: Seq[(DenseVector[Double],DenseVector[Double])], epochs: Int, miniBatchSize: Int, eta: Double, lmbda: Double = 0.0, testData:Seq[(DenseVector[Double],DenseVector[Double])] = Nil): Unit ={
     val trainingDataLength: Int = trainingData.length
     for(j <- 0 until epochs){
       Random.shuffle(trainingData)
@@ -65,7 +65,7 @@ class Network(networkDefinition: List[Int]) {
         for(k <- (0 until trainingDataLength by miniBatchSize).toList) yield trainingData.slice(k, k + miniBatchSize)
 
       for(miniBatch <- miniBatches)
-        updateMiniBatch(miniBatch, eta)
+        updateMiniBatch(miniBatch, eta, lmbda, trainingDataLength)
 
       if (testData != Nil)
         println("Epoch " + j + ": " + evaluate(testData) + "/" + testData.length)
@@ -74,7 +74,7 @@ class Network(networkDefinition: List[Int]) {
     }
   }
 
-  def updateMiniBatch(miniBatch: Seq[(DenseVector[Double],DenseVector[Double])], eta: Double): Unit = {
+  def updateMiniBatch(miniBatch: Seq[(DenseVector[Double],DenseVector[Double])], eta: Double, lmbda: Double, n: Int): Unit = {
     var nabla_b: ArrayBuffer[DenseVector[Double]] = for(b <- biases) yield DenseVector.zeros[Double](b.length)
     var nabla_w: ArrayBuffer[DenseMatrix[Double]] = for(w <- weights) yield DenseMatrix.zeros[Double](w.rows, w.cols)
 
@@ -89,7 +89,7 @@ class Network(networkDefinition: List[Int]) {
       nabla_b = for((nb, dnb) <- nabla_b.zip(delta_nabla_b)) yield nb+dnb
       nabla_w = for((nw, dnw) <- nabla_w.zip(delta_nabla_w)) yield nw+dnw
     }
-    weights = for((w, nw) <- weights.zip(nabla_w)) yield w-(eta/miniBatch.length)*nw
+    weights = for((w, nw) <- weights.zip(nabla_w)) yield (1-eta*(lmbda/n))*w-(eta/miniBatch.length)*nw //w-(eta/miniBatch.length)*nw
     biases = for((b, nb) <- biases.zip(nabla_b)) yield b-(eta/miniBatch.length)*nb
   }
 
@@ -110,7 +110,7 @@ class Network(networkDefinition: List[Int]) {
       activations += activation
     }
     //backward pass
-    var delta = costDerivative(activations.reverse.head, y) :* sigmoidPrime(zs.reverse.head)
+    var delta = costDerivative(activations.reverse.head, y) //:* sigmoidPrime(zs.reverse.head)
     nabla_b(nabla_b.length-1) = delta
     nabla_w(nabla_w.length-1) = delta * activations(activations.length-2).t
     for(l <- 2 until numberOfLayers){
@@ -126,12 +126,27 @@ class Network(networkDefinition: List[Int]) {
 
   def evaluate(testData: Seq[(DenseVector[Double],DenseVector[Double])]) = {
 
-    val testResult:Seq[(Int, DenseVector[Double])] = for((x,y) <- testData) yield (argmax(feedforward(x)), y)
+    val testResult:Seq[(Int, Int)] = for((x,y) <- testData) yield (argmax(feedforward(x)), argmax(y))
     var result = 0;
-    for((x,y) <- testResult; if y(x) == 1.0){
+    for((x,y) <- testResult; if y == x){
       result+=1;
     }
     result
+  }
+
+  /*
+  *
+  *
+  * Return the cost associated with an output ``a`` and desired output
+  * ``y``.  Note that np.nan_to_num is used to ensure numerical
+  * stability.  In particular, if both ``a`` and ``y`` have a 1.0
+  * in the same slot, then the expression (1-y)*np.log(1-a)
+  * returns nan.  The np.nan_to_num ensures that that is converted
+  * to the correct value (0.0).
+  *
+  */
+  def crossEntropyCost(a: DenseVector[Double], y: DenseVector[Double]) = {
+
   }
 
   def costDerivative(outputActivations: DenseVector[Double], y: DenseVector[Double]): DenseVector[Double] = {
